33314f5ec97a7271c99bc1b70f2860190a4b926c
dukeOfDebug <asquella@alejandros-MBP.home>
2024-03-15 11:12:00 +0100

    step 1 - create user

--- src/application/habit/create-habit.command-handle.ts
+++ src/application/habit/create-habit.command-handle.ts
@@ -2,18 +2,20 @@ import { Injectable } from "@nestjs/common";
 import { Habit } from "../../domain/habit/habit";
 import { HabitRepository } from "../../domain/habit/habit.repository";
 import { CreateHabitCommand } from "./create-habit.command";
+import { HabitValidationService } from "./validation-habit.service";
 
 
 @Injectable()
 export class CreateHabitCommandHandler{
+    private validationService: HabitValidationService;
 
-    constructor(private readonly repository: HabitRepository){}
+    constructor(private readonly repository: HabitRepository){
+        this.validationService = new HabitValidationService(repository);
+    }
 
     handle(command: CreateHabitCommand){
-        const existingHabit = this.repository.findDuplicateForUser(command.userId, command.name);
-        if (existingHabit) {
-            throw new Error("Habit already exists for this user.");
-        }
+        this.validationService.validateCreation(command);
+
         const habit = new Habit(
             command.id,
             command.name,
